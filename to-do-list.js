be3978164207078c6a2df28488214d005f25c1a1
Maria Tzelepidou <tzelepidoumaria@gmail.com>
2024-01-27 20:05:18 +0000

    added localstorage

--- to-do-list.js
+++ to-do-list.js
@@ -1,36 +1,73 @@
 const things = document.getElementById("things");
 const mylist = document.getElementById("mylist");
 
+let listItems = JSON.parse(localStorage.getItem("listItems"));
+
+function renderList(list) {
+    for (let i = 0; i < list.length; i++) {
+        renderItem(list[i]);
+    }
+}
+
+function clearList() {
+    const elements = [];
+
+    for (const child of mylist.children) {
+        elements.push(child);
+    }
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].remove();
+    }
+}
+
 function AddThings() {
     const thingsValue = things.value;
 
     if (thingsValue.length == 0) {
-        console.log("Maria");
         return
     }
 
+    clearList();
+    listItems.push(thingsValue);
+    renderList(listItems);
+
+    localStorage.setItem("listItems", JSON.stringify(listItems));
+
+    things.value = "";
+}
+
+function renderItem(itemName) {
     const div = document.createElement("div");
     div.setAttribute("class", "container");
 
     const span = document.createElement("span");
-    span.innerHTML = thingsValue;
+    span.innerHTML = itemName;
     div.appendChild(span);
 
     const del = document.createElement("div");
     del.setAttribute("class", "span-delete");
     del.onclick = () => {
-        Delete(div);
+        Delete(itemName);
     };
     del.textContent = "x";
     div.appendChild(del);
 
     mylist.appendChild(div);
-
-    things.value = "";
 }
 
+function Delete(itemName) {
+    listItems = listItems.filter(item => item != itemName);
+    localStorage.setItem("listItems", JSON.stringify(listItems));
+    clearList();
+    renderList(listItems);
+}
 
+document.getElementById("main-screen").style.display = 'none';
+renderList(listItems);
 
-function Delete(div) {
-    mylist.removeChild(div);
+function hide() {
+    var finger = document.querySelector(".hidden-point-finger");
+    finger.style.display = 'none';
+    document.getElementById("main-screen").style.display = '';
+    document.getElementById("finger-screen").style.display = 'none';
 }
